f48ba571fc98074f4aed4d41c338e53b844a64de
Suyogya Luitel <Lsuyogya@gmail.com>
2025-08-18 15:47:35 +0545

    more img replacements and color fixes

---
+++ replace_selected_images.cmd
@@ -0,0 +1,19 @@
+@echo off
+copy /Y "E:\Development\Codeilo\HANGLOOSE\assets\img\banner\banner-5\temp_cropped_banner-5.jpg" "E:\Development\Codeilo\HANGLOOSE\assets\img\banner\banner-5\banner-5.jpg"
+del "E:\Development\Codeilo\HANGLOOSE\assets\img\banner\banner-5\temp_cropped_banner-5.jpg"
+copy /Y "E:\Development\Codeilo\HANGLOOSE\assets\img\breadcrumb\temp_cropped_breadcrumb.jpg" "E:\Development\Codeilo\HANGLOOSE\assets\img\breadcrumb\breadcrumb.jpg"
+del "E:\Development\Codeilo\HANGLOOSE\assets\img\breadcrumb\temp_cropped_breadcrumb.jpg"
+copy /Y "E:\Development\Codeilo\HANGLOOSE\assets\img\breadcrumb\temp_cropped_breadcrumb-2.jpg" "E:\Development\Codeilo\HANGLOOSE\assets\img\breadcrumb\breadcrumb-2.jpg"
+del "E:\Development\Codeilo\HANGLOOSE\assets\img\breadcrumb\temp_cropped_breadcrumb-2.jpg"
+copy /Y "E:\Development\Codeilo\HANGLOOSE\assets\img\breadcrumb\temp_cropped_breadcrumb-3.jpg" "E:\Development\Codeilo\HANGLOOSE\assets\img\breadcrumb\breadcrumb-3.jpg"
+del "E:\Development\Codeilo\HANGLOOSE\assets\img\breadcrumb\temp_cropped_breadcrumb-3.jpg"
+copy /Y "E:\Development\Codeilo\HANGLOOSE\assets\img\chose\su\temp_cropped_thumb.jpg" "E:\Development\Codeilo\HANGLOOSE\assets\img\chose\su\thumb.jpg"
+del "E:\Development\Codeilo\HANGLOOSE\assets\img\chose\su\temp_cropped_thumb.jpg"
+copy /Y "E:\Development\Codeilo\HANGLOOSE\assets\img\chose\su\temp_cropped_thumb-2.png" "E:\Development\Codeilo\HANGLOOSE\assets\img\chose\su\thumb-2.png"
+del "E:\Development\Codeilo\HANGLOOSE\assets\img\chose\su\temp_cropped_thumb-2.png"
+copy /Y "E:\Development\Codeilo\HANGLOOSE\assets\img\hero\tu-2\temp_cropped_bg.jpg" "E:\Development\Codeilo\HANGLOOSE\assets\img\hero\tu-2\bg.jpg"
+del "E:\Development\Codeilo\HANGLOOSE\assets\img\hero\tu-2\temp_cropped_bg.jpg"
+copy /Y "E:\Development\Codeilo\HANGLOOSE\assets\img\chose\su\temp_cropped_thumb.jpg" "E:\Development\Codeilo\HANGLOOSE\assets\img\chose\su\thumb.jpg"
+del "E:\Development\Codeilo\HANGLOOSE\assets\img\chose\su\temp_cropped_thumb.jpg"
+echo Image replacement complete.
+pause
